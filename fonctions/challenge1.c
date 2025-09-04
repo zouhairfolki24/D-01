084e97ca4ec638b136cfd7a3de6154d006c6560d
Zou23hair <zouhair.folki@gmail.com>
2025-09-04 17:32:44 +0100

    three commit

--- fonctions/challenge1.c
+++ fonctions/challenge1.c
@@ -5,15 +5,16 @@ int somme(int number1, int number2) {
 }
 
 int main () {
-    // int number1, number2;
+    int number1;
+    int number2;
 
-    // printf("Number 1: ");
-    // scanf("%d", &number1);
+    printf("Number 1: ");
+    scanf("%d", &number1);
+    
+    printf("Number 2: ");
+    scanf("%d", &number2);
 
-    // printf("Number 1: ");
-    // scanf("%d", &number1);
-
-    printf("La somme est: %d", somme(20, 10));
+    printf("La somme est: %d", somme(number1, number2));
 
     return 0;
 }
---
+++ fonctions/challenge3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int maximum (int number1, int number2) {
+    int max = number1;
+    if (number2 > number1) {
+        max = number2;
+    }
+    return max;
+}
+
+int main() {
+
+    int number1; 
+    int number2;
+    
+    printf("Saisir le nombre 1: ");
+    scanf("%d", &number1);
+
+    printf("Saisir le nombre 1: ");
+    scanf("%d", &number1);
+
+    printf("le maximum est: %d", maximum(number1, number2));
+
+    return 0;
+}
